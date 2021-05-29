8a3947ce06dcbc0d765dae98f1636e21d7c21f35
skeyan <eyan0749@gmail.com>
2021-05-29 01:03:10 -0400

    Make each ghost keep track of its own movement

--- pacman.py
+++ pacman.py
@@ -9,7 +9,7 @@ blue = (0,0,255)
 green = (0,255,0)
 red = (255,0,0)
 purple = (255,0,255)
-yellow   = ( 255, 255,   0)
+yellow   = ( 255, 255,0)
 
 myIcon=pygame.image.load('images/pacman.png')
 pygame.display.set_icon(myIcon)
@@ -200,27 +200,52 @@ class Player(pygame.sprite.Sprite):
 
 #Inheritime Player klassist
 class Ghost(Player):
-    # Change the speed of the ghost
-    def changespeed(self,list,ghost,turn,steps,l):
-      try:
-        z=list[turn][2]
-        if steps < z:
-          self.change_x=list[turn][0]
-          self.change_y=list[turn][1]
-          steps+=1
+  # Ghost type
+  type = ""
+
+  # Movement variables
+  turn = 0
+  steps = 0
+
+    # Constructor function
+  def __init__(self,x,y,filename,ghostType):
+      # Call the parent's constructor
+      pygame.sprite.Sprite.__init__(self)
+  
+      # Set height, width
+      self.image = pygame.image.load(filename).convert()
+
+      # Make our top-left corner the passed-in location.
+      self.rect = self.image.get_rect()
+      self.rect.top = y
+      self.rect.left = x
+      self.prev_x = x
+      self.prev_y = y
+
+      # Initialize type
+      self.type = ghostType
+
+  # Change the speed of the ghost
+  def changespeed(self,list,ghost,turn,steps,l):
+    try:
+      z=list[turn][2]
+      if steps < z:
+        self.change_x=list[turn][0]
+        self.change_y=list[turn][1]
+        steps+=1
+      else:
+        if turn < l:
+          turn+=1
+        elif ghost == "clyde":
+          turn = 2
         else:
-          if turn < l:
-            turn+=1
-          elif ghost == "clyde":
-            turn = 2
-          else:
-            turn = 0
-          self.change_x=list[turn][0]
-          self.change_y=list[turn][1]
-          steps = 0
-        return [turn,steps]
-      except IndexError:
-         return [0,0]
+          turn = 0
+        self.change_x=list[turn][0]
+        self.change_y=list[turn][1]
+        steps = 0
+      return [turn,steps]
+    except IndexError:
+        return [0,0]
 
 Pinky_directions = [
 [0,-30,4],
@@ -339,10 +364,6 @@ pygame.init()
 # Create an 606x606 sized screen
 screen = pygame.display.set_mode([606, 606])
 
-# This is a list of 'sprites.' Each block in the program is
-# added to this list. The list is managed by a class called 'RenderPlain.'
-
-
 # Set the title of the window
 pygame.display.set_caption('Pacman')
 
@@ -355,10 +376,10 @@ background = background.convert()
 # Fill the screen with a black background
 background.fill(black)
 
-
-
+# Initialize game clock
 clock = pygame.time.Clock()
 
+# Set game font
 pygame.font.init()
 font = pygame.font.Font("freesansbold.ttf", 24)
 
@@ -372,6 +393,7 @@ c_w = 303+(32-16) #Clyde width
 
 def startGame():
 
+  # Set up the sprites
   all_sprites_list = pygame.sprite.RenderPlain()
 
   block_list = pygame.sprite.RenderPlain()
@@ -384,38 +406,24 @@ def startGame():
 
   gate = setupGate(all_sprites_list)
 
-
-  p_turn = 0
-  p_steps = 0
-
-  b_turn = 0
-  b_steps = 0
-
-  i_turn = 0
-  i_steps = 0
-
-  c_turn = 0
-  c_steps = 0
-
-
   # Create the player paddle object
   Pacman = Player( w, p_h, "images/pacman.png" )
   all_sprites_list.add(Pacman)
   pacman_collide.add(Pacman)
    
-  Blinky=Ghost( w, b_h, "images/Blinky.png" )
+  Blinky=Ghost( w, b_h, "images/Blinky.png", "blinky" )
   monsta_list.add(Blinky)
   all_sprites_list.add(Blinky)
 
-  Pinky=Ghost( w, m_h, "images/Pinky.png" )
+  Pinky=Ghost( w, m_h, "images/Pinky.png", "pinky" )
   monsta_list.add(Pinky)
   all_sprites_list.add(Pinky)
    
-  Inky=Ghost( i_w, m_h, "images/Inky.png" )
+  Inky=Ghost( i_w, m_h, "images/Inky.png", "inky" )
   monsta_list.add(Inky)
   all_sprites_list.add(Inky)
    
-  Clyde=Ghost( c_w, m_h, "images/Clyde.png" )
+  Clyde=Ghost( c_w, m_h, "images/Clyde.png", "clyde" )
   monsta_list.add(Clyde)
   all_sprites_list.add(Clyde)
 
@@ -443,12 +451,11 @@ def startGame():
               all_sprites_list.add(block)
 
   bll = len(block_list)
+  maxGhost = len(monsta_list) * 4;
 
   score = 0
 
   done = False
-
-  i = 0
 
   while done == False:
       # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
@@ -481,29 +488,29 @@ def startGame():
       # ALL GAME LOGIC SHOULD GO BELOW THIS COMMENT
       Pacman.update(wall_list,gate)
 
-      returned = Pinky.changespeed(Pinky_directions,False,p_turn,p_steps,pl)
-      p_turn = returned[0]
-      p_steps = returned[1]
-      Pinky.changespeed(Pinky_directions,False,p_turn,p_steps,pl)
-      Pinky.update(wall_list,False)
+      for monsta in monsta_list:
+        if monsta.type == "pinky":
+          returned = monsta.changespeed(Pinky_directions,False,monsta.turn,monsta.steps,pl)
+          monsta.turn = returned[0]
+          monsta.steps = returned[1]
+          monsta.changespeed(Pinky_directions,False,monsta.turn,monsta.steps,pl)
+        elif monsta.type == "blinky":
+          returned = monsta.changespeed(Blinky_directions,False,monsta.turn,monsta.steps,bl)
+          monsta.turn = returned[0]
+          monsta.steps = returned[1]
+          monsta.changespeed(Blinky_directions,False,monsta.turn,monsta.steps,bl)
+        elif monsta.type == "inky":
+          returned = monsta.changespeed(Inky_directions,False,monsta.turn,monsta.steps,il)
+          monsta.turn = returned[0]
+          monsta.steps = returned[1]
+          monsta.changespeed(Inky_directions,False,monsta.turn,monsta.steps,il)
+        else:
+          returned = Clyde.changespeed(Clyde_directions,"clyde",monsta.turn,monsta.steps,cl)
+          monsta.turn = returned[0]
+          monsta.steps = returned[1]
+          monsta.changespeed(Clyde_directions,"clyde",monsta.turn,monsta.steps,cl)
 
-      returned = Blinky.changespeed(Blinky_directions,False,b_turn,b_steps,bl)
-      b_turn = returned[0]
-      b_steps = returned[1]
-      Blinky.changespeed(Blinky_directions,False,b_turn,b_steps,bl)
-      Blinky.update(wall_list,False)
-
-      returned = Inky.changespeed(Inky_directions,False,i_turn,i_steps,il)
-      i_turn = returned[0]
-      i_steps = returned[1]
-      Inky.changespeed(Inky_directions,False,i_turn,i_steps,il)
-      Inky.update(wall_list,False)
-
-      returned = Clyde.changespeed(Clyde_directions,"clyde",c_turn,c_steps,cl)
-      c_turn = returned[0]
-      c_steps = returned[1]
-      Clyde.changespeed(Clyde_directions,"clyde",c_turn,c_steps,cl)
-      Clyde.update(wall_list,False)
+        monsta.update(wall_list,False)
 
       # See if the Pacman block has collided with anything.
       blocks_hit_list = pygame.sprite.spritecollide(Pacman, block_list, True)
@@ -528,9 +535,11 @@ def startGame():
       if score == bll:
         doNext("Congratulations, you won!",145,all_sprites_list,block_list,monsta_list,pacman_collide,wall_list,gate)
 
+      # If we collided with a monster, game over
       monsta_hit_list = pygame.sprite.spritecollide(Pacman, monsta_list, False)
 
-      if monsta_hit_list:
+      # Game over if number of monsters exceeds 32 * 4 (original number of ghosts)
+      if len(monsta_list) > maxGhost:
         doNext("Game Over",235,all_sprites_list,block_list,monsta_list,pacman_collide,wall_list,gate)
 
       # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
